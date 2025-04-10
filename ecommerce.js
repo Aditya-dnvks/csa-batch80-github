9c7a2b910dd80cbbc34aeef8bbad2fb7e6651bfe
Aditya Dasari <aditya.dnvks@gmail.com>
2025-04-10 13:19:49 +0530

    Batch 80

--- ecommerce.js
+++ ecommerce.js
@@ -1,15 +1,15 @@
 async function fetchData() {
   try {
-    console.log("API call start");
+    // console.log("API call start");
     const data = await fetch("https://fakestoreapi.com/products");
     const response = await data.json();
-    console.log(response, "data from API");
+    // console.log(response, "data from API");
 
     displayCards(response);
   } catch (err) {
     console.log(err);
   } finally {
-    console.log("API call completed");
+    // console.log("API call completed");
   }
 }
 
@@ -58,3 +58,74 @@ function displayCards(array) {
     `;
   }
 }
+
+// Advaned JS concepts:
+// 1. Currying
+// 2. Closures
+// 3. this --> Call, apply and bind
+
+// function multiply(a, b, c) {
+//   return a * b * c;
+// }
+
+// function multiply2(a) {
+//   return function (b) {
+//     return function (c) {
+//       return a * b * c;
+//     };
+//   };
+// }
+
+// //funcitonal programming
+
+// console.log(multiply(5, 3, 2));
+// console.log(multiply2(5)(3)(2));
+
+// function add(a) {
+//   return function (b) {
+//     return a + b;
+//   };
+// }
+
+// const addTwo = add(5)(6);
+// console.log(addTwo);
+
+// function counter() {
+//   let counter = 0;
+//   return function () {
+//     counter++;
+//     return counter;
+//   };
+// }
+// const add = counter();
+
+// console.log(add()); //1
+// console.log(add()); //2
+// console.log(add()); //3
+
+//lexical scope
+
+//call, apply, bind
+
+// const obj = {
+//   name: "CSA",
+//   age: 24,
+//   greet: function (city, country) {
+//     return `Hi I am ${this.name}. My age is ${this.age}. ${city}, ${country}`;
+//   },
+// };
+
+// const obj2 = {
+//   name: "John Doe",
+//   age: 25,
+// };
+
+// console.log(obj.greet.call(obj2, "Hyderabad", "India"));
+// console.log(obj.greet.apply(obj2, ["Hyderabad", "India"]));
+
+// const newMethod = obj.greet.bind(obj2);
+// console.log(newMethod("Hyderabad", "India"));
+
+console.log(age);
+var age = 20;
+console.log(window);
